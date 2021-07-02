6a70cf9cb3e173fb07965b26891b559c15ba87f9
Carlos Andres Lozano Arroyo <clozanoarroyo@gmail.com>
2021-07-01 21:32:49 -0500

    Uso de la clase CrudRepository

---
+++ src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
@@ -0,0 +1,19 @@
+package com.platzi.market.persistence.crud;
+
+import com.platzi.market.persistence.entity.Producto;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductoCrudRepository extends CrudRepository<Producto,Integer > {
+
+    /*
+    * Using Query methods
+    *  @see https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods
+    * */
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
+
+}
